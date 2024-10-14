188221ffae8135e1c803dc54781216f678f87456
jaycando <148449719+jaycando@users.noreply.github.com>
2024-10-14 13:12:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='langchain_jay',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         # 필요한 패키지 목록
